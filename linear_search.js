0e05d3352bac49330703513552e0406875784e0e
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-03-28 23:54:16 +0800

    done with linear search and global linear search

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,15 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let index = -1;
+  for (let i = 0 ; i < values.length ; i++ ) {
+    // console.log(values[i]);
+    if (values[i] == target){
+      index = i;
+      break;
+    }
+  }
+  return index;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +26,14 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let index = [];
+  for (let i = 0 ; i < values.length ; i++ ) {
+    // console.log(values[i]);
+    if (values[i] == target){
+      index.push(i);
+    }
+  }
+  return index;
 }
 
 let banana_arr = "banana".split("");
