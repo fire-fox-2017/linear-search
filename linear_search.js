14baa82f1d60a9988155d9fb82bdbf5510b84f5d
stedyyulius <stedyyulius@gmail.com>
2017-03-29 20:33:26 +0700

    first commit

--- linear_search.js
+++ linear_search.js
@@ -6,21 +6,16 @@ let linearSearch = (target, values) => {
 var box = [];
 for (var i = 0; i < values.length; i++){
   if (values[i] === target){
-    box.push(target)
+    return i;
   }
 }
-{
-  if (box.length === 0){
-    return -1;
-  }
-return box.length;
-}
+return -1;
   }
 
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
-console.log(linearSearch(18, random_numbers));
+console.log(linearSearch(72, random_numbers));
 // 2
 console.log(linearSearch(9, random_numbers));
 // -1
@@ -33,8 +28,6 @@ let globalLinearSearch = (target, values) => {
     if (target === values[i]){
       kotak.push(i)
     }
-
-
   }
     return kotak;
 }
