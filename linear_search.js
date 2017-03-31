db97e497dc424b046576401f5e0be455e503d99f
edim dendy &   "name": null <pair+edimdendidiwangga.zanisis@hacktiv8.com>
2017-03-31 14:44:28 +0700

    linear search- edim

--- linear_search.js
+++ linear_search.js
@@ -3,10 +3,20 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  let num = 0;
+
+  for (var i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      num = i
+      break
+    } else {
+      num = -1
+    }
+  }
+  return num
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
